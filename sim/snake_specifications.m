da3ac58549c8db974801461390db4f7737f957b0
Atussa Koushan <atussak@stud.ntnu.no>
2019-10-23 14:36:20 +0200

    correct obstacle force

--- sim/snake_specifications.m
+++ sim/snake_specifications.m
@@ -1,6 +1,6 @@
 % User defined specifications
 
-global n
+global n q0
 
 n = 3; % number of links
 
@@ -8,4 +8,7 @@ n = 3; % number of links
 
 % link length
 
-% initial joint positions
+q0    = zeros(N,1); % initial joint positions
+q0(1) = pi/6;
+q0(2) = -pi/2;
+q0(3) = pi/3;
