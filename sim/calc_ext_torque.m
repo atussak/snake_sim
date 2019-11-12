2e1f4b64c9a3379c05027253094f5a49b61f1c91
Atussa Koushan <atussak@stud.ntnu.no>
2019-11-12 14:52:05 +0100

    comment

--- sim/calc_ext_torque.m
+++ sim/calc_ext_torque.m
@@ -10,30 +10,30 @@ function tau_ext = calc_ext_torque(M, C, tau, q, qd, qd_prev, in_contact)
         if in_contact(i)
             all_Jc  = Jc_func(q');
             Jc      = all_Jc(:,:,i);
-            all_Jcd = Jcd_func(q', qd');
-            Jcd     = all_Jcd(:,:,i);
-
-            EF      = obstacle_coords(i,:);
-
-            aq      = Jcd*qd;
-
-            r_d     = (Jc*qd);
-            EF_d    = r_d';
-            arF     = EF_d*r_d;
-
-            b1      = tau - C;
-            b2      = -EF*aq - arF;
-
-            k1 = EF*Jc;
-            k2 = pinv(M);
-            k3 = Jc'*EF';
-            K       = pinv(k1*k2*k3);
-
-            f_ext   = -K*(b2 - EF*Jc*k2*b1);
-            f = [0; f_ext];
-            tau_ext = tau_ext + Jc'*f;
-            
-            h = 0.01; m = 0.5;
+%             all_Jcd = Jcd_func(q', qd');
+%             Jcd     = all_Jcd(:,:,i);
+% 
+%             EF      = obstacle_coords(i,:);
+% 
+%             aq      = Jcd*qd;
+% 
+%             r_d     = (Jc*qd);
+%             EF_d    = r_d';
+%             arF     = EF_d*r_d;
+% 
+%             b1      = tau - C;
+%             b2      = -EF*aq - arF;
+% 
+%             k1 = EF*Jc;
+%             k2 = pinv(M);
+%             k3 = Jc'*EF';
+%             K       = pinv(k1*k2*k3);
+% 
+%             f_ext   = -K*(b2 - EF*Jc*k2*b1);
+%             f = [0; f_ext];
+%             tau_ext = tau_ext + Jc'*f;
+%             
+            h = 0.01; m = 1;
             xd_prev = Jc*qd_prev;
             xd = Jc*qd;
             xdd = (xd-xd_prev)/h;
