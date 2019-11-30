99e5cdd82d3cc292b93fca475443a1b3a633dd4a
Atussa Koushan <atussak@stud.ntnu.no>
2019-11-30 14:22:13 +0100

    Update get_path_reference_angle2.m

--- sim/get_path_reference_angle2.m
+++ sim/get_path_reference_angle2.m
@@ -12,25 +12,8 @@ function q_ref = get_path_reference_angle2(prev_point, point, proj_point, q_sim)
     q_diff = acos((b^2 + c^2 - a^2)/(2*b*c));
 
     % Increment or decrement the reference?
-    
-%     x = point(1);
-%     y = point(2);
-%     proj_x = proj_point(1);
-%     proj_y = proj_point(2);
-%     
-%     if abs(proj_y - y) > abs(proj_x - y)
-%         if y > proj_y
-%             q_ref = q_sim - q_diff;
-%         else
-%             q_ref = q_sim + q_diff; 
-%         end
-%     else
-%         if x > proj_x
-%             q_ref = q_sim - q_diff;
-%         else
-%             q_ref = q_sim + q_diff; 
-%         end
-%     end
+    % --> try both and choose the one that gives the best result
+    % Brute force, but robust, method
     
     q_diff_sgn = [-q_diff, q_diff];
     link_vec = point - prev_point;
@@ -52,10 +35,5 @@ function q_ref = get_path_reference_angle2(prev_point, point, proj_point, q_sim)
             q_ref = q_sim + phi;
         end
     end
-    
-    
-    
-    
-    
     
 end
